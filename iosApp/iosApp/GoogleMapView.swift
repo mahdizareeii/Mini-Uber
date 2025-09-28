f3f36f8a957643bb91202f5b746fac9cd7e95584
mahdizareei <m.zareei@jabama.com>
2025-09-28 20:37:11 +0330

    fix map marker in both platform

--- iosApp/iosApp/GoogleMapView.swift
+++ iosApp/iosApp/GoogleMapView.swift
@@ -10,50 +10,80 @@ import ComposeApp
 class MapViewController: UIViewController, KmpMap, GMSMapViewDelegate {
     private let onCameraChanged: (MapState.LatLng) -> KotlinUnit
     private var mapView: GMSMapView!
-
+    
     init(onCameraChanged: @escaping (MapState.LatLng) -> KotlinUnit) {
         self.onCameraChanged = onCameraChanged
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let camera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 1)
         mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.delegate = self
         view.addSubview(mapView)
     }
-
+    
     func updateState(state: MapState) {
         mapView.clear()
         var bounds = GMSCoordinateBounds()
-
+        
         if let start = state.startPoint {
-            let startMarker = GMSMarker(position: CLLocationCoordinate2D(latitude: start.latitude, longitude: start.longitude))
+            let startMarker = GMSMarker(
+                position: CLLocationCoordinate2D(
+                    latitude: start.latLng.latitude,
+                    longitude: start.latLng.longitude
+                )
+            )
             startMarker.icon = GMSMarker.markerImage(with: .green)
+            startMarker.title = start.title
+            startMarker.snippet = start.snipped
             startMarker.map = mapView
             bounds = bounds.includingCoordinate(startMarker.position)
         }
-
+        
         if let end = state.endPoint {
-            let endMarker = GMSMarker(position: CLLocationCoordinate2D(latitude: end.latitude, longitude: end.longitude))
+            let endMarker = GMSMarker(
+                position: CLLocationCoordinate2D(
+                    latitude: end.latLng.latitude,
+                    longitude: end.latLng.longitude
+                )
+            )
             endMarker.icon = GMSMarker.markerImage(with: .red)
+            endMarker.title = end.title
+            endMarker.snippet = end.snipped
             endMarker.map = mapView
             bounds = bounds.includingCoordinate(endMarker.position)
         }
-
+        
         if state.startPoint != nil && state.endPoint != nil {
             mapView.animate(with: GMSCameraUpdate.fit(bounds, withPadding: 100))
         } else if let start = state.startPoint {
-            mapView.animate(with: GMSCameraUpdate.setTarget(CLLocationCoordinate2D(latitude: start.latitude, longitude: start.longitude), zoom: 10))
+            mapView.animate(
+                with: GMSCameraUpdate.setTarget(
+                    CLLocationCoordinate2D(
+                        latitude: start.latLng.latitude,
+                        longitude: start.latLng.longitude
+                    ),
+                    zoom: 10
+                )
+            )
         } else if let end = state.endPoint {
-            mapView.animate(with: GMSCameraUpdate.setTarget(CLLocationCoordinate2D(latitude: end.latitude, longitude: end.longitude), zoom: 10))
+            mapView.animate(
+                with: GMSCameraUpdate.setTarget(
+                    CLLocationCoordinate2D(
+                        latitude: end.latLng.latitude,
+                        longitude: end.latLng.longitude
+                    ),
+                    zoom: 10
+                )
+            )
         }
     }
     
